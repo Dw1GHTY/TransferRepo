44b910165427e12892a7d2b61004e742c1a590fb
marko <mih.mare@gmail.com>
2025-10-23 19:01:24 +0200

    ti si smrda prda

---
+++ DS/grpc/vezba/Server/Services/DatabaseService.cs
@@ -0,0 +1,47 @@
+﻿namespace MessagingService.Services;
+
+public class DatabaseService
+{
+    private static DatabaseService messageQueue;
+    private static object lockObj = new object();
+    private static List<Message> messages;
+    public DatabaseService()
+    {
+        messages = new List<Message>();
+    }
+
+    public static DatabaseService getInstance()
+    {
+        if (messageQueue == null)
+        {
+            lock (lockObj)
+            {
+                if (messageQueue == null)
+                    messageQueue = new DatabaseService();
+            }
+        }
+        return messageQueue;
+    }
+
+    public void AddMessage(Message msg)
+    {
+        messages.Add(msg);
+    }
+
+    public List<Message> GetMessages()
+    {
+        return messages;
+    }
+
+    public Message FindAndDeleteMessage(Id id) 
+    {
+        Message msg = messages.Find(m => m.Id == id.Id_);
+        if(msg != null)
+            messages.Remove(msg);
+
+        return msg;
+    }
+
+
+}
+
---
+++ DS/grpc/vezba/Server/Services/MessageQueueService.cs
@@ -0,0 +1,31 @@
+﻿
+using Grpc.Core;
+using Google.Protobuf.WellKnownTypes;
+using MessagingService;
+using MessagingService.Services;
+
+public class MessageQueueService : MessagingService.MessageQueueService.MessageQueueServiceBase
+{
+
+    public override Task<Empty> SendMessage(Message message, ServerCallContext context) 
+    {
+        DatabaseService db = DatabaseService.getInstance();
+        db.AddMessage(message);
+
+        return Task.FromResult(new Empty());
+    }
+
+    public override Task<Message> DeleteMessage(Id id, ServerCallContext context) 
+    {
+        DatabaseService db = DatabaseService.getInstance();
+        Message msgToDelete = db.FindMessage(id);
+
+        if (msgToDelete != null) 
+        {
+            db.De
+            return Task.FromResult(msgToDelete);
+        }
+    }
+
+}
+
